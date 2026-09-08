34ec314c8e2043563d2180d3ab0cc3eeb0903759
Sanket Dhole <sanket.dhole1997@gmail.com>
2026-09-08 08:51:16 +0530

    feat: initialize Spring Boot project with Gradle wrapper and essential dependencies

---
+++ src/main/java/com/phaselume/torana/ToranaApplication.java
@@ -0,0 +1,13 @@
+package com.phaselume.torana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToranaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToranaApplication.class, args);
+	}
+
+}
